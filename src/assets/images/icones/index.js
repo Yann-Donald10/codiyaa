c898ff2b1a5c67a7e3f67680ff7464a10511a3da
sapissap <pauline.libert@epfedu.fr>
2026-01-13 15:21:40 +0100

    Ajout perso dos

--- src/assets/images/icones/index.js
+++ src/assets/images/icones/index.js
@@ -3,6 +3,8 @@ import Femme from './Femme.png';
 import DrapeauHaut from './drapeau_haut.png'
 import DrapeauBas from './drapeau_bas.png'
 import Luteur from './Luteur.png'
+import HommeDos from './HommeDos.png'
+import FemmeDos from './FemmeDos.png'
 
 // ... Ajoutez ici toutes les autres images de décors ...
 
@@ -11,5 +13,7 @@ export {
     Femme,
     DrapeauHaut,
     DrapeauBas,
-    Luteur 
+    Luteur,
+    HommeDos,
+    FemmeDos
 };
